6ea60242eb2172b3bb44817c149f0c9ab36e8d2a
Lincoln <lincolny@ngis.com.au>
2021-11-19 16:33:37 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Login from './components/Login';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <div className="App">
+      hello world
+      <Profile />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
